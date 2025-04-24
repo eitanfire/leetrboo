f36d9b2403d9b050c09b413ae32d23bdbd353f0f
Eitan Fire <eitanfire@gmail.com>
2025-04-23 23:51:49 -0600

    experiment with modal

--- packages/web/src/pages/SignInPage.tsx
+++ packages/web/src/pages/SignInPage.tsx
@@ -1,23 +1,28 @@
+// src/pages/SignInPage.tsx
 import { SignInForm } from "../components/AuthForms";
 import { Image, Text, Button } from "@mantine/core";
 import Brand from "../assets/leetrboo_brand_bg.png";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import { JoinCompetitionModal } from "../components/JoinCompetitionModal";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 const SignInPage: React.FC = () => {
-  const [isJoinModalOpen, setIsJoinModalOpen] = useState(false);
+  // Use state to track modal visibility
+  const [joinModalOpened, setJoinModalOpened] = useState(false);
 
-  const openModal = () => {
-    console.log("Join button clicked - Opening modal");
-    setIsJoinModalOpen(true);
-  };
+  // Use callback to ensure consistent reference
+  const handleOpenModal = useCallback(() => {
+    console.log("Opening modal - setting state to true");
+    setJoinModalOpened(true);
+  }, []);
 
-  const closeModal = () => {
-    console.log("Closing modal");
-    setIsJoinModalOpen(false);
-  };
+  const handleCloseModal = useCallback(() => {
+    console.log("Closing modal - setting state to false");
+    setJoinModalOpened(false);
+  }, []);
+
+  console.log("SignInPage rendering. Modal open state:", joinModalOpened);
 
   return (
     <Container fluid className="auth-container">
@@ -38,29 +43,21 @@ const SignInPage: React.FC = () => {
         </Col>
       </Row>
 
-      <Button
-        className="mt-3"
-        color="teal"
-        onClick={openModal}
-      >
+      {/* Button to open the modal */}
+      <Button className="mt-3" color="teal" onClick={handleOpenModal}>
         Join a Competition
       </Button>
 
-      {isJoinModalOpen && (
-        <JoinCompetitionModal
-          opened={isJoinModalOpen}
-          onClose={closeModal}
-        />
-      )}
+      {/* Modal component with opened prop */}
+      <JoinCompetitionModal
+        opened={joinModalOpened}
+        onClose={handleCloseModal}
+      />
 
       <Row className="row mt-3 d-flex">
         <Col className="col vstack">
           {/* Link to Sign Up page */}
-          <Button
-            className="sign-up-btn"
-            component={Link}
-            to="/signup"
-          >
+          <Button className="sign-up-btn" component={Link} to="/signup">
             Sign up
           </Button>
           <Button
